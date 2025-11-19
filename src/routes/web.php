0f2b341d76ebc5ce9aba4cf846aa9e33f9936688
Dev A <s.k.1117.a.b.c@gmail.com>
2025-11-19 13:11:49 +0900

    clothesコントローラの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SwaggerTestController;
+use App\Http\Controllers\ClothesController;
 
-Route::get('/api/hello', [SwaggerTestController::class, 'hello']);
-Route::post('/api/users', [SwaggerTestController::class, 'store']);
+
+// Route::get('/api/hello', [SwaggerTestController::class, 'hello']);
+// Route::post('/api/users', [SwaggerTestController::class, 'store']);
+
